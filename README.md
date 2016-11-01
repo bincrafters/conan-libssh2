065c39c0744e83a7586b86960388b475806aacdd
Alex Kucher <a.kucher@avantize.com>
2016-11-02 01:40:53 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # conan-libssh2
-Conan.io package for libssh2 library
+
+[Conan.io](https://conan.io) package for [libssh2](https://github.com/libssh2/libssh2) library
+
+The packages generated with this **conanfile** can be found in [conan.io](https://www.conan.io/source/libssh2/1.8.0/Wi3ard/stable).
+
+## Build packages
+
+Download conan client from [Conan.io](https://conan.io) and run:
+
+    $ python build.py
+
+## Upload packages to server
+
+    $ conan upload libssh2/1.8.0@Wi3ard/stable --all
+
+## Reuse the packages
+
+### Basic setup
+
+    $ conan install libssh2/1.8.0@Wi3ard/stable
+    
+### Project setup
+
+If you handle multiple dependencies in your project is better to add a *conanfile.txt*
+    
+    [requires]
+    libssh2/1.8.0@Wi3ard/stable
+
+    [options]
+    libssh2:shared=True # False
+    
+    [generators]
+    txt
+    cmake
+
+Complete the installation of requirements for your project running:
+
+    conan install . 
+
+Project setup installs the library (and all his dependencies) and generates the files *conanbuildinfo.txt* and *conanbuildinfo.cmake* with all the paths and variables that you need to link with your dependencies.
---
+++ conanfile.py
@@ -0,0 +1,83 @@
+from conans import ConanFile, CMake
+import os
+import shutil
+
+class libssh2Conan(ConanFile):
+    name = "libssh2"
+    version = "1.8.0"
+    url="https://github.com/Wi3ard/conan-libssh2"
+    generators = "cmake", "txt"
+    settings = "os", "compiler", "build_type", "arch"
+    exports = "cmake/*"
+    short_paths = True
+    options = {"shared": [True, False],
+               "enable_zlib": [True, False],
+               "enable_crypt_none": [True, False],
+               "enable_mac_none": [True, False],
+               "crypto_backend": ["none", "OpenSSL"],
+               }
+    default_options = "shared=False", \
+        "enable_zlib=True", \
+        "enable_crypt_none=False", \
+        "enable_mac_none=False", \
+        "crypto_backend=OpenSSL"
+
+    def source(self):
+        self.run("git clone https://github.com/libssh2/libssh2.git")
+        self.run("cd libssh2 && git checkout tags/libssh2-1.8.0")
+
+        self.output.info("Copying CMakeLists.txt")
+        os.unlink("libssh2/CMakeLists.txt")
+        shutil.move("cmake/CMakeLists.txt", "libssh2")
+
+    def config(self):
+        if self.options.enable_zlib:
+            self.requires.add("zlib/1.2.8@lasote/stable", private=False)
+        if self.options.crypto_backend == "OpenSSL":
+            self.requires.add("OpenSSL/1.0.2i@lasote/stable", private=False)
+
+    def build(self):
+        cmake = CMake(self.settings)
+
+        cmake_options = []
+        for option_name in self.options.values.fields:
+            activated = getattr(self.options, option_name)
+            the_option = "%s=" % option_name.upper()
+            if option_name == "shared":
+               the_option = "BUILD_SHARED_LIBS=ON" if activated else "BUILD_SHARED_LIBS=OFF"
+            elif option_name == "enable_zlib":
+               the_option = "ENABLE_ZLIB_COMPRESSION=ON" if activated else "ENABLE_ZLIB_COMPRESSION=OFF"
+            elif option_name == "enable_crypt_none":
+               the_option = "ENABLE_CRYPT_NONE=ON" if activated else "ENABLE_CRYPT_NONE=OFF"
+            elif option_name == "enable_mac_none":
+               the_option = "ENABLE_MAC_NONE=ON" if activated else "ENABLE_MAC_NONE=OFF"
+            elif option_name == "crypto_backend":
+                if activated == "OpenSSL":
+                    the_option = "CRYPTO_BACKEND=OpenSSL" if activated else "CRYPTO_BACKEND="
+            else:
+               the_option += "ON" if activated else "OFF"
+            cmake_options.append(the_option)
+
+        cmake_cmd_options = " -D".join(cmake_options)
+
+        cmake_conf_command = 'cmake %s/libssh2 %s -DCMAKE_INSTALL_PREFIX:PATH=install -D%s' % (self.conanfile_directory, cmake.command_line, cmake_cmd_options)
+        self.output.warn(cmake_conf_command)
+        self.run(cmake_conf_command)
+
+        self.run("cmake --build . --target install %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="install/include")
+        self.copy("*.dll", dst="bin", src="install/bin")
+        self.copy("*.lib", dst="lib", src="install/lib")
+        self.copy("*.a", dst="lib", src="install/lib")
+        self.copy("*.so", dst="lib", src="install/lib")
+        self.copy("*.*", dst="lib/cmake/libssh2", src="install/lib/cmake/libssh2")
+        self.copy("*.*", dst="lib/pkgconfig", src="install/lib/pkgconfig")
+
+    def package_info(self):
+        self.cpp_info.libs = ["libssh2"]
+
+        if self.settings.os == "Windows":
+            if not self.options.shared:
+                self.cpp_info.libs.append('ws2_32')

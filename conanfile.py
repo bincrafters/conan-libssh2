63470e123da12e4a81a25bdf9d8eaf02e5dc9203
Alex Kucher <a.kucher@avantize.com>
2016-11-18 23:12:08 +0200

    Fixed OS X build

--- conanfile.py
+++ conanfile.py
@@ -66,12 +66,17 @@ class libssh2Conan(ConanFile):
 
         self.run("cmake --build . --target install %s" % cmake.build_config)
 
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+
     def package(self):
         self.copy("*.h", dst="include", src="install/include")
         self.copy("*.dll", dst="bin", src="install/bin")
         self.copy("*.lib", dst="lib", src="install/lib")
         self.copy("*.a", dst="lib", src="install/lib")
         self.copy("*.so*", dst="lib", src="install/lib")
+        self.copy("*.dylib", dst="lib", src="install/lib")
         self.copy("*.*", dst="lib/cmake/libssh2", src="install/lib/cmake/libssh2")
         self.copy("*.*", dst="lib/pkgconfig", src="install/lib/pkgconfig")
 

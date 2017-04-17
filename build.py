affd58d34437764bd2a3769a5526d77c95c76799
theirix <theirix@gmail.com>
2017-04-17 18:17:43 +0300

    Disable OpenSSL efence

--- build.py
+++ build.py
@@ -3,4 +3,8 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="libssh2:shared", pure_c=True)
+    noefence_builds = []
+    for settings, options in builder.builds:
+        noefence_builds.append([settings, dict(options.items() + [('OpenSSL:no_electric_fence', True)])])
+    builder.builds = noefence_builds
     builder.run()
